ac140f335fe27529ae7561f2d4c8a1d06a252ca3
jrcoak <josephrcoakley@gmail.com>
2024-09-13 15:04:59 -0400

    Create create-jira-issue.js

---
+++ .github/create-jira-issue.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+(async () => {
+  try {
+    const response = await axios.post('https://jellythijs.atlassian.net/rest/api/3/issue', {
+      fields: {
+        project: { key: 'AGT' }, // Your Jira project key
+        summary: 'Issue Created By Joe GH Action', // Customize summary
+        issuetype: { name: 'Task' }, // Specify issue type
+      }
+    }, {
+      headers: {
+        'Authorization': `Basic ${process.env.JIRA_API_TOKEN}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(response.data.key);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+    process.exit(1);
+  }
+})();
